6b896a8a882937d54be8874b6a88e9a2bc101cf2
SathemBite <toni.lesnikov.98@mail.ru>
2018-01-16 19:15:49 +0300

    initial commit

---
+++ src/main/java/MathFunctions.java
@@ -0,0 +1,18 @@
+
+
+
+public class MathFunctions {
+
+    public static int fact(int n){
+        int res = 1;
+
+        for (; n > 1; n--){
+            res *= n;
+        }
+
+        return res;
+    }
+
+    public static
+
+}
