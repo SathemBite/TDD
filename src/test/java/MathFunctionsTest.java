493843f65da4252c03c98bd8468a419241661405
SathemBite <toni.lesnikov.98@mail.ru>
2018-01-16 19:43:19 +0300

    deleted useless imports

--- src/test/java/MathFunctionsTest.java
+++ src/test/java/MathFunctionsTest.java
@@ -1,12 +1,9 @@
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
-
 import static org.junit.jupiter.api.Assertions.*;
 
-class MathFunctionsTest {
 
+class MathFunctionsTest {
 
     @ParameterizedTest
     @CsvSource({"0, 1", "3, 6", "5, 120"})
